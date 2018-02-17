aee33f1689253057621a1f8b1ebfb52e4b54ae57
КимМ <@227_10>
2018-02-17 18:23:31 +0600

    Add project files.

---
+++ ConsoleApplication3/Header.h
@@ -0,0 +1,6 @@
+int f(int m2, int m1, int m0);
+int sum(int a, int b);
+double sum(double a, double b);
+double razn(double a, double b);
+void katet(double &a, double &b, double c, double q);
+void katet_1(double *a, double *b, double c, double q);
